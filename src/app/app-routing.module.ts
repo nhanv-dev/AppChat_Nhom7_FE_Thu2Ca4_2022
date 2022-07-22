f49d445f8b2b48c17fba74a73e6f99684d9fb112
nhanv <tthanhnhan1512@gmail.com>
2022-07-22 14:45:14 +0700

    Update services, authen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import {RegisterComponent} from "./register/register.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, title: 'App Chat - Trang chủ', canActivate: []},
+  {path: 'home', component: HomeComponent, title: 'App Chat - Trang chủ', canActivate: [AuthenticationGuard]},
   {path: 'login', component: LoginComponent, title: 'App Chat - Đăng nhập'},
   {path: 'register', component: RegisterComponent, title: 'App Chat - Đăng ký'},
-  {path: '', component: HomeComponent, title: 'App Chat - Trang chủ', canActivate: []},
+  {path: '', component: HomeComponent, title: 'App Chat - Trang chủ', canActivate: [AuthenticationGuard]},
   {path: '**', component: LoginComponent, title: 'App Chat - Đăng nhập'},
 ];
 
